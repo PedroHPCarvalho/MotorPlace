b64904ff9f05077be3157dd2e3802524a7a28444
Pedro <pedroh.fokus@gmail.com>
2025-07-23 21:26:05 -0300

    fix: Alterar Spring do Core e BFF

---
+++ core-back/src/test/java/com/motorplace/back_core/BackCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorplace.back_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ web-back/src/test/java/com/motorplace/back_web/BackWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorplace.back_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
